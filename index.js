4bf70a710c450c6c0a158dad74b76a5320d3872e
SergeyBeltsin <beltsins89@gmail.com>
2021-10-10 00:37:55 +0300

    create read stream

--- index.js
+++ index.js
@@ -7,17 +7,39 @@ const path = require('path');
 
 const web3 = new Web3(`https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`);
 
-const checkCode = async (code) => {
+const newWhitelist = [['wallet', 'q', 'isContract']];
+
+const checkCode = async (address) => {
   try {
-    const code = await web3.eth.getCode(code);
-    console.log(code);
+    const code = await web3.eth.getCode(address);
+
+    return code !== '0x';
   } catch (e) {
     console.log(e);
   }
 };
 
+const handleError = (error) => {
+  console.error(error);
+};
+
+const handleEnd = () => {
+  console.log(newWhitelist);
+  csv.writeToPath('whitelist-formatted.csv', newWhitelist);
+};
+
+const handleTransformRow = async (row) => {
+  const isCodeValid = await checkCode(row.wallet);
+
+  newWhitelist.push([
+    row.wallet,
+    row.q,
+    isCodeValid ? 'TRUE' : 'FALSE',
+  ]);
+};
+
 fs.createReadStream(path.resolve(__dirname, 'whitelist.csv'))
   .pipe(csv.parse({ headers: true }))
-  .on('error', (error) => console.error(error))
-  .on('data', (row) => console.log(row))
-  .on('end', (rowCount) => console.log(`Parsed ${rowCount} rows`));
+  .on('error', handleError)
+  .on('data', handleTransformRow)
+  .on('end', handleEnd);
