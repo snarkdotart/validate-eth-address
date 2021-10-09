51bd8b4e04b23097d44da352a0042926799dd2a5
SergeyBeltsin <beltsins89@gmail.com>
2021-10-09 23:58:36 +0300

    setup web3 and fastCsv, read data from .csv file

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+require('dotenv').config();
 
+const Web3 = require('web3');
+const csv = require('fast-csv');
+const fs = require('fs');
+const path = require('path');
+
+const web3 = new Web3(`https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`);
+
+const checkCode = async (code) => {
+  try {
+    const code = await web3.eth.getCode(code);
+    console.log(code);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+fs.createReadStream(path.resolve(__dirname, 'whitelist.csv'))
+  .pipe(csv.parse({ headers: true }))
+  .on('error', (error) => console.error(error))
+  .on('data', (row) => console.log(row))
+  .on('end', (rowCount) => console.log(`Parsed ${rowCount} rows`));
