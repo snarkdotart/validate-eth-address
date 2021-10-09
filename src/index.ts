63a48ebac80878a4c13f5a184c28d97a20214ed3
SergeyBeltsin <beltsins89@gmail.com>
2021-10-10 01:29:27 +0300

    rewrite to typescript

--- src/index.ts
+++ src/index.ts
@@ -6,38 +6,46 @@ const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
 
+type Whitelist = Array<[string, string, string]>;
+interface IWhitelistItem {
+  wallet: string;
+  q: string;
+}
+
 const web3 = new Web3(`https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`);
-const newWhitelist = [['wallet', 'q', 'isContract']];
+const newWhitelist: Whitelist = [['wallet', 'q', 'isContract']];
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-rl.question("Write .csv filename here (without extension): ", (filename) => {
+rl.question("Write .csv filename here (without extension): ", (filename: string): void => {
   rl.close();
 
-  const checkCode = async (address) => {
+  const checkCode = async (address: string): Promise<boolean> => {
     try {
-      const code = await web3.eth.getCode(address);
+      const code: string = await web3.eth.getCode(address);
 
       return code !== '0x';
     } catch (e) {
       console.log(e);
+
+      return false;
     }
   };
 
-  const handleError = (error) => {
+  const handleError = (error: string): void => {
     console.error(error);
   };
 
-  const handleEnd = () => {
-    csv.writeToPath(`${filename}-formatted.csv`, newWhitelist);
+  const handleEnd = (): void => {
+    csv.writeToPath(`${__dirname}/${filename}-formatted.csv`, newWhitelist);
   };
 
-  const handleTransformRow = async (row) => {
+  const handleTransformRow = async (row: IWhitelistItem): Promise<void> => {
     stream.pause();
 
-    const isCodeValid = await checkCode(row.wallet);
+    const isCodeValid: boolean = await checkCode(row.wallet);
     newWhitelist.push([
       row.wallet,
       row.q,
@@ -52,4 +60,4 @@ rl.question("Write .csv filename here (without extension): ", (filename) => {
     .on('error', handleError)
     .on('data', handleTransformRow)
     .on('end', handleEnd);
-})
+});
