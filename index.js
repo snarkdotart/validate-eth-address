4b6ffb20f07bd2316e7762daa31c7ef545350220
SergeyBeltsin <beltsins89@gmail.com>
2021-10-10 01:02:31 +0300

    make script

--- index.js
+++ index.js
@@ -4,42 +4,52 @@ const Web3 = require('web3');
 const csv = require('fast-csv');
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 
 const web3 = new Web3(`https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`);
-
 const newWhitelist = [['wallet', 'q', 'isContract']];
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-const checkCode = async (address) => {
-  try {
-    const code = await web3.eth.getCode(address);
+rl.question("Write .csv filename here (without extension): ", (filename) => {
+  rl.close();
 
-    return code !== '0x';
-  } catch (e) {
-    console.log(e);
-  }
-};
+  const checkCode = async (address) => {
+    try {
+      const code = await web3.eth.getCode(address);
 
-const handleError = (error) => {
-  console.error(error);
-};
+      return code !== '0x';
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-const handleEnd = () => {
-  console.log(newWhitelist);
-  csv.writeToPath('whitelist-formatted.csv', newWhitelist);
-};
+  const handleError = (error) => {
+    console.error(error);
+  };
 
-const handleTransformRow = async (row) => {
-  const isCodeValid = await checkCode(row.wallet);
+  const handleEnd = () => {
+    csv.writeToPath(`${filename}-formatted.csv`, newWhitelist);
+  };
 
-  newWhitelist.push([
-    row.wallet,
-    row.q,
-    isCodeValid ? 'TRUE' : 'FALSE',
-  ]);
-};
+  const handleTransformRow = async (row) => {
+    stream.pause();
 
-fs.createReadStream(path.resolve(__dirname, 'whitelist.csv'))
-  .pipe(csv.parse({ headers: true }))
-  .on('error', handleError)
-  .on('data', handleTransformRow)
-  .on('end', handleEnd);
+    const isCodeValid = await checkCode(row.wallet);
+    newWhitelist.push([
+      row.wallet,
+      row.q,
+      isCodeValid ? 'TRUE' : 'FALSE',
+    ]);
+
+    stream.resume();
+  };
+
+  const stream = fs.createReadStream(path.resolve(__dirname, `${filename}.csv`))
+    .pipe(csv.parse({ headers: true }))
+    .on('error', handleError)
+    .on('data', handleTransformRow)
+    .on('end', handleEnd);
+})
